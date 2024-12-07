6e60f8e7c8ca27e0f3ff121c93fe8610f9ce10b1
Pinkyuu <maksim7.25@mail.ru>
2024-12-07 18:25:17 +0700

    lab_9 Add music portal

--- lab_9/index.php
+++ lab_9/index.php
@@ -9,11 +9,23 @@ if (!isLoggedIn()) {
     exit;
 }
 
+echo "<h2>Добро пожаловать на музыкальный портал!</h2>";
+
+echo "<div style='text-align: center; margin-top: 20px;'>
+        <img src='gif/minion.gif' alt='Музыкальная анимация' />
+      </div>";
+
+// Добавляем ссылку на музыкальные рекомендации
+echo " <a href='yandex.php'>Рекомендации музыки в Yandex</a>";
+echo " <a href='vk.php'>Рекомендации музыки в VK</a>";
+
+
 if (isAdmin()) {
     echo " <a href='admin.php'>Панель администратора</a>";
 } else {
     echo " <a href='profile.php'>Панель пользователя</a>";
 }
+
 ?>
 
 <a href="logout.php">Выйти</a>
